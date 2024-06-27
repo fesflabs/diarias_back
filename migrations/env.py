016264ae001ea4348cb7c878b98c92b74ba26090
Paloma <paloma.sousa.santos@hotmail.com>
2024-06-27 10:31:10 -0300

    fest:migrations

--- migrations/env.py
+++ migrations/env.py
@@ -5,19 +5,20 @@ from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
-from models.models import Funcionario, RegistroBanco, Estado, Cidade
 from core.configs import settings
 from decouple import config
 import os
 from dotenv import load_dotenv
 from alembic import context
 
+from models.models import Funcionario, RegistroBanco, Estado, Cidade
+
 load_dotenv()
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
+
 config = context.config
 
-config.get_main_option('sqlalchemy.url', os.getenv('DATABASE_URL'))
+config.set_main_option('sqlalchemy.url', os.getenv('DATABASE_URL'))
+
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
@@ -36,17 +37,7 @@ target_metadata = settings.DBBaseModel.metadata
 
 
 def run_migrations_offline() -> None:
-    """Run migrations in 'offline' mode.
-
-    This configures the context with just a URL
-    and not an Engine, though an Engine is acceptable
-    here as well.  By skipping the Engine creation
-    we don't even need a DBAPI to be available.
-
-    Calls to context.execute() here emit the given string to the
-    script output.
-
-    """
+   
     url = config.get_main_option("sqlalchemy.url")
     context.configure(
         url=url,
