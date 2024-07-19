df71ac8967e52de5124c98a0bde78576d548ed14
Matheus Jesus dos Santos <Matheus.Jesus@fesfsus.ba>
2024-07-19 12:37:27 -0300

    feat: Implementado endpoint calcular_diarias para lidar com diferentes tipos de solicitações: solicitação, complementação e devolução. Valida informações de data, hora, cidade e codigo da SD.

--- api/v1/endpoints/diaria.py
+++ api/v1/endpoints/diaria.py
@@ -1,33 +1,138 @@
 from fastapi import Depends, APIRouter, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
-from typing import List
 from core.deps import get_session, validate_form_token
-from schema.diaria_schema import Trecho
-from utils.diarias_functions import calcular_valores, verificar_duracao_total
-from utils.sd_functions import gerar_numero_unico
+from schema.diaria_schema import Solicitacao, SDRequest
+from utils.diarias_functions import calcular_valores, verificar_duracao_total, validar_data, validar_hora
+from utils.sd_functions import gerar_numero_unico, gerar_data_hora_sd, validar_codigo_sd
 
 router = APIRouter()
 
+
 @router.post("/calcular")
 async def calcular_diarias(
-    trechos: List[Trecho],
+    request: Solicitacao,
     db: AsyncSession = Depends(get_session),
     user_id: str = Depends(validate_form_token)
-    ):
-    
+):
+
+    trechos = request.trechos
+    tipo_sd = request.tipo_sd
+    codigo_sd = request.codigo_sd
+    valor_sd = request.valor_sd
+
     if not trechos:
-        raise HTTPException(status_code=400, detail="Lista de trechos está vazia.")
-    
+        raise HTTPException(
+            status_code=400, detail="Lista de trechos está vazia."
+        )
+
+    for trecho in trechos:
+        if not validar_data(trecho.dt_saida) or not validar_data(trecho.dt_retorno):
+            raise HTTPException(
+                status_code=400, detail=f"Formato de data inválido em um dos trechos: {trecho}"
+            )
+        if not validar_hora(trecho.hr_saida) or not validar_hora(trecho.hr_retorno):
+            raise HTTPException(
+                status_code=400, detail=f"Formato de hora inválido em um dos trechos: {trecho}"
+            )
+        if not trecho.cidade_origem or not trecho.cidade_destino:
+            raise HTTPException(
+                status_code=400, detail="Cidades de origem e destino são obrigatórias."
+            )
+        if not trecho.estado_origem or not trecho.estado_destino:
+            raise HTTPException(
+                status_code=400, detail="Estados de origem e destino são obrigatórios."
+            )
+
     verificar_duracao_total(trechos)
     diarias_completas, diarias_simples, valor_total = await calcular_valores(trechos, db)
-    
-    # Gerar o número único SD
-    sd = await gerar_numero_unico(db)
+
+    match tipo_sd:
+        case "solicitação":
+            return {
+                "diarias_completas": diarias_completas,
+                "diarias_simples": diarias_simples,
+                "valor_total": round(valor_total, 2),
+                "user_id": user_id
+            }
+        case "complementação":
+            if not codigo_sd or valor_sd is None:
+                raise HTTPException(
+                    status_code=400, detail="Campos 'codigo_sd' e 'valor_sd' são obrigatórios para complementação."
+                )
+            if valor_sd >= valor_total:
+                raise HTTPException(
+                    status_code=400, detail="O valor_sd deve ser menor que o valor_total para complementação."
+                )
+            if not validar_codigo_sd(codigo_sd):
+                raise HTTPException(
+                    status_code=400, detail="Formato de 'codigo_sd' inválido. Deve ser 00000/ano."
+                )
+            valor_complementacao = valor_total - valor_sd
+            return {
+                "diarias_completas": diarias_completas,
+                "diarias_simples": diarias_simples,
+                "valor_total": round(valor_total, 2),
+                "valor_sd": round(valor_sd, 2),
+                "valor_complementacao": round(valor_complementacao, 2),
+                "user_id": user_id
+            }
+        case "devolução":
+            if not codigo_sd or valor_sd is None:
+                raise HTTPException(
+                    status_code=400, detail="Campos 'codigo_sd' e 'valor_sd' são obrigatórios para devolução."
+                )
+            if valor_sd <= valor_total:
+                raise HTTPException(
+                    status_code=400, detail="O valor_sd deve ser maior que o valor_total para devolução."
+                )
+            if not validar_codigo_sd(codigo_sd):
+                raise HTTPException(
+                    status_code=400, detail="Formato de 'codigo_sd' inválido. Deve ser 00000/ano."
+                )
+            valor_devolucao = valor_sd - valor_total
+            return {
+                "diarias_completas": diarias_completas,
+                "diarias_simples": diarias_simples,
+                "valor_total": round(valor_total, 2),
+                "valor_sd": round(valor_sd, 2),
+                "valor_devolucao": round(valor_devolucao, 2),
+                "user_id": user_id
+            }
+        case _:
+            raise HTTPException(
+                status_code=400, detail="Tipo de solicitação inválido."
+            )
+
+
+@router.post("/gerar_sd")
+async def gerar_sd(
+    request: SDRequest,
+    db: AsyncSession = Depends(get_session),
+    user_id: str = Depends(validate_form_token)
+):
+    tipo_sd = request.tipo_sd
+    codigo_sd = request.codigo_sd
+
+    if tipo_sd not in ['solicitação', 'complementação', 'devolução']:
+        raise HTTPException(
+            status_code=400, detail="Tipo de solicitação inválido. Use 'solicitação', 'complementação' ou 'devolução'.")
+
+    if tipo_sd == 'solicitação':
+        sd = await gerar_numero_unico(db)
+        data_hora_sd = gerar_data_hora_sd()
+    else:
+        if not codigo_sd:
+            raise HTTPException(
+                status_code=400, detail="O parâmetro 'codigo_sd' é obrigatório para tipos 'complementação' e 'devolução'.")
+        if not validar_codigo_sd(codigo_sd):
+            raise HTTPException(
+                status_code=400, detail="Formato de 'codigo_sd' inválido. Deve ser 00000/ano."
+            )
+        sd = codigo_sd
+        data_hora_sd = None
 
     return {
-        "diarias_completas": diarias_completas,
-        "diarias_simples": diarias_simples,
-        "valor_total": valor_total,
-        "sd": sd,
+        "codigo_sd": sd,
+        "data_hora_sd": data_hora_sd,
         "user_id": user_id
     }
