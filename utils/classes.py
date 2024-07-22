3306d905832156620232064b5732f8f92589858d
itsmairar <itsmairar@outlook.com>
2024-07-22 15:43:14 -0300

    Correção da validação de cpf

--- utils/classes.py
+++ utils/classes.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, validator,Field
+from pydantic import BaseModel, validator, Field
 from validate_docbr import CPF
 import re
 
@@ -8,8 +8,14 @@ class FuncionarioCPF(BaseModel):
     @validator('cpf')
     def validar_cpf(cls, cpf: str) -> str:
         cpf_numerico = re.sub(r'\D', '', cpf)
+        
+        # Verificar o tamanho do CPF
+        if len(cpf_numerico) != 11:
+            raise ValueError('O CPF deve conter 11 dígitos numéricos')
+        
+        # Verificar se o CPF é válido
         cpf_validator = CPF()
-        if cpf_validator.validate(cpf_numerico):
-            return cpf_numerico
-        else:
-            raise ValueError('CPF inválido')  
+        if not cpf_validator.validate(cpf_numerico):
+            raise ValueError('CPF inválido')
+        
+        return cpf_numerico
