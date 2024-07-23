5b8f031c687319b51ddd7d4098aec3bac0060b5c
Matheus Jesus dos Santos <Matheus.Jesus@fesfsus.ba>
2024-07-23 10:16:05 -0300

    feat: atualizar roteador principal para incluir endpoint de banco

--- api/v1/api.py
+++ api/v1/api.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter
 
-from api.v1.endpoints import funcionario, diaria, localidades
+from api.v1.endpoints import funcionario, diaria, localidades, banco
 
 api_router = APIRouter()
 
 api_router.include_router(funcionario.router, prefix='/funcionario', tags= ['funcionario'])
 api_router.include_router(diaria.router, prefix='/diaria', tags=['diaria'])
 api_router.include_router(localidades.router, prefix='/localidades', tags=['localidades'])
+api_router.include_router(banco.router, prefix='/banco', tags=['banco'])
